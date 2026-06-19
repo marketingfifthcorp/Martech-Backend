ab0677105b26f7aba1f00d8e2cf8215858a4dec1
Marketing Fifth Corp <marketing.fifthcorp@gmail.com>
2026-06-19 23:27:52 +0500

    adjusting

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3001;
   await app.listen(port);
-  console.log(`🚀 Martech API running on http://localhost:${port}/api/v1`);
+  console.log(`🚀 Martech API running on new http://localhost:${port}/api/v1`);
 }
 
 bootstrap();
